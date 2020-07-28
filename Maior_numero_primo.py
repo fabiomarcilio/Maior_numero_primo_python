3cdb17ff94581c7c3ab6d494ebf5bbcfb6f35fca
Fabio Marcilio <fabiomarcilio@msn.com>
2020-07-28 17:38:53 -0300

    transformado o calculo em uma função

--- Maior_numero_primo.py
+++ Maior_numero_primo.py
@@ -1,20 +1,22 @@
 numero = int(input('Entre com um número inteiro:'))
-i = 1
-x = 1
-while (i <= numero):
-    result = 0
-    x = 1
-    while(x <= i):
-        if(i % x == 0):
-            result = result + 1
-            x = x + 1
-        else:
-            x = x + 1
 
-    if (result > 2):
-        i = i + 1
-    else:
-        primo = i
-        i = i + 1
-print(primo)
+def maiorPrimo(numero):
+    i = 1
+    x = 1
+    while (i <= numero):
+        result = 0
+        x = 1
+        while(x <= i):
+            if(i % x == 0):
+                result = result + 1
+                x = x + 1
+            else:
+                x = x + 1
+        if (result > 2):
+            i = i + 1
+        else:
+            primo = i
+            i = i + 1
+    print(primo)
     
+maiorPrimo(numero)
